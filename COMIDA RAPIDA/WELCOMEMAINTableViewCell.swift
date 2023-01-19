4702b2069763af8cb30acf54fe4deb8f006f8c26
LUIS <luis.enrique.enc@gmail.com>
2023-01-18 18:54:44 -0600

    [ADD] SE AGREGAN LAS 3 PRIMERAS VISTAS DE LA APLICACION

---
+++ COMIDA RAPIDA/WELCOMEMAINTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  WELCOMEMAINTableViewCell.swift
+//  COMIDA RAPIDA
+//
+//  Created by LUIS GONZALEZ on 16/01/23.
+//
+
+import UIKit
+
+class WELCOMEMAINTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- COMIDA RAPIDA/WELCOMEMAINViewController.swift
+++ COMIDA RAPIDA/WELCOMEMAINViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WELCOMEMAINViewController.swift
 //  COMIDA RAPIDA
 //
 //  Created by LUIS GONZALEZ on 16/01/23.
@@ -7,14 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WELCOMEMAINViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
+        
     }
+    
+   
 
     
 
 }
-
